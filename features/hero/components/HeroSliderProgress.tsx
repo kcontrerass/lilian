28bba4ac1e8d4120921b366682ba1a79030ee912
wkastro <wcastro@aumenta.do>
2026-07-02 13:39:01 -0600

    feat: add HeroSliderProgress component

---
+++ features/hero/components/HeroSliderProgress.tsx
@@ -0,0 +1,9 @@
+export default function HeroSliderProgress() {
+  return (
+    <div className="absolute bottom-10 w-[90%] max-w-[1130px] mx-auto z-10">
+      <div className="w-full h-[13px] bg-white/50 backdrop-blur-md rounded-full overflow-hidden">
+        <div className="h-full bg-lilian-orange w-[25%] rounded-full"></div>
+      </div>
+    </div>
+  );
+}
