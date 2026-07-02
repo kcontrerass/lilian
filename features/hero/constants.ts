30106dadc800914a405652645204ff014ce8c538
wkastro <wcastro@aumenta.do>
2026-07-02 13:38:26 -0600

    feat: add hero constants

---
+++ features/hero/constants.ts
@@ -0,0 +1,10 @@
+export const HERO_IMAGES = {
+  background: "/assets/1cac87c18c1468d2888af44c8b9eaf900c8113fd.png",
+} as const;
+
+export const HERO_TEXTS = {
+  headlinePrefix: "Hecho con",
+  highlightedWord: "amor",
+  headlineSuffix: "y horneado para ti.",
+  cta: "Ordena en Pedidos Ya",
+} as const;
