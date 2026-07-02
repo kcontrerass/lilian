e93912ee5818ca65b5116c1505978a290f6fdb7a
wkastro <wcastro@aumenta.do>
2026-07-02 13:43:45 -0600

    feat: add mission constants

---
+++ features/mission/constants.ts
@@ -0,0 +1,20 @@
+export const MISSION_IMAGES = {
+  main: "/assets/a92c92b1efcd065cb45b45ee8829992627c1d38d.png",
+  dots: "/assets/35d89ec71c4436f403a57a0e9f4398e046db8aba.svg",
+  whiteLogo: "/assets/e01406bd27c2a730171ccbfdf33cc1ef24c179da.svg",
+  sticker: "/assets/de26de4eadb76eac4b1836ae5c8f3afc248ab9a6.svg",
+  backgroundShape: "/assets/95483b2bb3649200ddd8d916da6ca91a9821833c.svg",
+} as const;
+
+export const MISSION_TEXTS = {
+  titlePart1: "Cada ",
+  highlightedWord: "receta",
+  titlePart2: " tiene",
+  titlePart3: "una historia",
+  missionLabel: "Nuestra Misión:",
+  missionText:
+    "Atender y satisfacer las necesidades de nuestros clientes, brindando productos y servicios de alta calidad, pertenecientes a la industria pastelera, con un personal altamente motivado y capacitado, contribuyendo al desarrollo del país y de nuestros colaboradores.",
+  visionLabel: "Nuestra Visión:",
+  visionText:
+    "Queremos que en cada bocado que disfrutes te lleves una experiencia única del sabor, que por una vida ha venido a mis manos y poder compartir estas vivencias a través de recetas únicas.",
+} as const;
